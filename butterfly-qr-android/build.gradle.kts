dcfaaab3f0118dc96aaae585d9453e74380b4322
Brady Svedin <brady@lightningkite.com>
2021-03-10 12:27:17 -0700

    Fixed the search for certs and passwords with regards to publishing. It now uses System environment variables and Props, it was using project properties instead of props from the local.properties which is incorrect. Applied the updated signing across all butterfly android projects.

--- butterfly-qr-android/build.gradle.kts
+++ butterfly-qr-android/build.gradle.kts
@@ -21,6 +21,35 @@ plugins {
 group = "com.lightningkite.butterfly"
 version = "0.1.1"
 
+val props = project.rootProject.file("local.properties").takeIf { it.exists() }?.inputStream()?.use { stream ->
+    Properties().apply { load(stream) }
+}
+val signingKey: String? = (System.getenv("SIGNING_KEY")?.takeUnless { it.isEmpty() }
+    ?: props?.getProperty("signingKey")?.toString())
+    ?.lineSequence()
+    ?.filter { it.trim().firstOrNull()?.let { it.isLetterOrDigit() || it == '=' || it == '/' || it == '+' } == true }
+    ?.joinToString("\n")
+val signingPassword: String? = System.getenv("SIGNING_PASSWORD")?.takeUnless { it.isEmpty() }
+    ?: props?.getProperty("signingPassword")?.toString()
+val useSigning = signingKey != null && signingPassword != null
+
+if (signingKey != null) {
+    if (!signingKey.contains('\n')) {
+        throw IllegalArgumentException("Expected signing key to have multiple lines")
+    }
+    if (signingKey.contains('"')) {
+        throw IllegalArgumentException("Signing key has quote outta nowhere")
+    }
+}
+
+val deploymentUser = (System.getenv("OSSRH_USERNAME")?.takeUnless { it.isEmpty() }
+    ?: props?.getProperty("ossrhUsername")?.toString())
+    ?.trim()
+val deploymentPassword = (System.getenv("OSSRH_PASSWORD")?.takeUnless { it.isEmpty() }
+    ?: props?.getProperty("ossrhPassword")?.toString())
+    ?.trim()
+val useDeployment = deploymentUser != null || deploymentPassword != null
+
 repositories {
     jcenter()
     mavenCentral()
@@ -93,89 +122,90 @@ afterEvaluate {
             }
         }
     }
-    signing {
+    if (useSigning) {
+        signing {
+            useInMemoryPgpKeys(signingKey, signingPassword)
+            sign(configurations.archives.get())
+        }
+    }
+}
+
+if (useDeployment) {
+    tasks.register("uploadSnapshot") {
+        group = "upload"
+        finalizedBy("uploadArchives")
+        doLast {
+            project.version = project.version.toString() + "-SNAPSHOT"
+        }
+    }
+
+    tasks.named<Upload>("uploadArchives") {
+        repositories.withConvention(MavenRepositoryHandlerConvention::class) {
+            mavenDeployer {
+                beforeDeployment {
+                    signing.signPom(this)
+                }
+            }
+        }
+
         val props = project.rootProject.file("local.properties").takeIf { it.exists() }?.inputStream()?.use { stream ->
             Properties().apply { load(stream) }
         }
-        val signingKey: String? = props?.getProperty("signingKey") ?: project.properties["signingKey"]?.toString()
-        val signingPassword: String? =
-            props?.getProperty("signingPassword") ?: project.properties["signingPassword"]?.toString()
-        useInMemoryPgpKeys(signingKey, signingPassword)
-        sign(configurations.archives.get())
-    }
-}
 
-tasks.register("uploadSnapshot"){
-    group="upload"
-    finalizedBy("uploadArchives")
-    doLast{
-        project.version = project.version.toString() + "-SNAPSHOT"
-    }
-}
-
-tasks.named<Upload>("uploadArchives") {
-    repositories.withConvention(MavenRepositoryHandlerConvention::class) {
-        mavenDeployer {
-            beforeDeployment {
-                signing.signPom(this)
-            }
-        }
-    }
-
-    val props = project.rootProject.file("local.properties").takeIf { it.exists() }?.inputStream()?.use { stream ->
-        Properties().apply { load(stream) }
-    }
-
-    repositories.withGroovyBuilder {
-        "mavenDeployer"{
-            "repository"("url" to "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/") {
-                "authentication"(
-                    "userName" to (props?.getProperty("ossrhUsername")
-                        ?: project.properties["ossrhUsername"]?.toString()),
-                    "password" to (props?.getProperty("ossrhPassword")
-                        ?: project.properties["ossrhPassword"]?.toString())
-                )
-            }
-            "snapshotRepository"("url" to "https://s01.oss.sonatype.org/content/repositories/snapshots/") {
-                "authentication"(
-                    "userName" to (props?.getProperty("ossrhUsername")
-                        ?: project.properties["ossrhUsername"]?.toString()),
-                    "password" to (props?.getProperty("ossrhPassword")
-                        ?: project.properties["ossrhPassword"]?.toString())
-                )
-            }
-            "pom" {
-                "project" {
-                    setProperty("name", "Butterfly-QR-Android")
-                    setProperty("packaging", "aar")
-                    setProperty(
-                        "description",
-                        "A barcode scanning extension to Butterfly-Android"
+        repositories.withGroovyBuilder {
+            "mavenDeployer"{
+                "repository"("url" to "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/") {
+                    "authentication"(
+                        "userName" to (props?.getProperty("ossrhUsername")
+                            ?: project.properties["ossrhUsername"]?.toString()),
+                        "password" to (props?.getProperty("ossrhPassword")
+                            ?: project.properties["ossrhPassword"]?.toString())
                     )
-                    setProperty("url", "https://github.com/lightningkite/butterfly-qr-android")
-
-                    "scm" {
-                        setProperty("connection", "scm:git:https://github.com/lightningkite/butterfly-qr-android.git")
+                }
+                "snapshotRepository"("url" to "https://s01.oss.sonatype.org/content/repositories/snapshots/") {
+                    "authentication"(
+                        "userName" to (props?.getProperty("ossrhUsername")
+                            ?: project.properties["ossrhUsername"]?.toString()),
+                        "password" to (props?.getProperty("ossrhPassword")
+                            ?: project.properties["ossrhPassword"]?.toString())
+                    )
+                }
+                "pom" {
+                    "project" {
+                        setProperty("name", "Butterfly-QR-Android")
+                        setProperty("packaging", "aar")
                         setProperty(
-                            "developerConnection",
-                            "scm:git:https://github.com/lightningkite/butterfly-qr-android.git"
+                            "description",
+                            "A barcode scanning extension to Butterfly-Android"
                         )
                         setProperty("url", "https://github.com/lightningkite/butterfly-qr-android")
-                    }
 
-                    "licenses" {
-                        "license"{
-                            setProperty("name", "The MIT License (MIT)")
-                            setProperty("url", "https://www.mit.edu/~amini/LICENSE.md")
-                            setProperty("distribution", "repo")
+                        "scm" {
+                            setProperty(
+                                "connection",
+                                "scm:git:https://github.com/lightningkite/butterfly-qr-android.git"
+                            )
+                            setProperty(
+                                "developerConnection",
+                                "scm:git:https://github.com/lightningkite/butterfly-qr-android.git"
+                            )
+                            setProperty("url", "https://github.com/lightningkite/butterfly-qr-android")
                         }
 
-                    }
-                    "developers"{
-                        "developer"{
-                            setProperty("id", "bjsvedin")
-                            setProperty("name", "Brady Svedin")
-                            setProperty("email", "brady@lightningkite.com")
+                        "licenses" {
+                            "license"{
+                                setProperty("name", "The MIT License (MIT)")
+                                setProperty("url", "https://www.mit.edu/~amini/LICENSE.md")
+                                setProperty("distribution", "repo")
+                            }
+
+                        }
+                        "developers"{
+                            "developer"{
+                                setProperty("id", "bjsvedin")
+                                setProperty("name", "Brady Svedin")
+                                setProperty("email", "brady@lightningkite.com")
+                            }
                         }
                     }
                 }
